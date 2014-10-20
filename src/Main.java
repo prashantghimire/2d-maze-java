0b48eadd575b29a8d0bc6dcefbd28092c39590a0
Prashant Ghimire <prashant.ghimire@selu.edu>
2014-10-20 13:13:46 -0500

    Eclipse project that can be imported and run.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+/**
+ * @author Prashant Ghimire
+ * Date : 11/25/2013
+ * Program Description :
+ * This class is the main class. It creates a new Maze object passing the file name as a command line argument. 
+ * It calls solve(int row, in col)
+ * method of Maze class to solve the instantiated maze.
+ */
+public class Main {
+	public static void main(String[] args) throws IOException{
+		try {
+			Maze mymaze = new Maze("maze2.txt"); // args [0] if you want filename from command line
+		} catch (Exception e) {
+			System.out.println("ERROR : "+e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
