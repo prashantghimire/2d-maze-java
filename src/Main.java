628d77c1b3cb54d5dbdceae7b0f44d2fe4c87139
Prashant Ghimire <prashant.ghimire@selu.edu>
2014-10-28 21:18:51 -0500

    Print statement after file output.

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,8 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		try {
-			Maze mymaze = new Maze("maze2.txt"); // args [0] if you want filename from command line
+			new Maze("maze2.txt"); // args [0] if you want filename from command line
+			System.out.println("File has been output!");
 		} catch (Exception e) {
 			System.out.println("ERROR : "+e.getMessage());
 			e.printStackTrace();
